a8a0d4d2889ef1aff1f8cd5ac2af427d58114f30
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-06-11 21:14:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  
+  def initialize 
+    
+    @roster = {}
+    
+  end
+  
+  def add_student (name, grade)
+    
+    @roster[name] = []
+    @roster[name] << grade
+    
+  end
+  
+  
+end
+
+school = School.new("Bayside High School")
+school.add_student("Zach Morris", 9)
+school.roster #= {}
+school.grade(9)
